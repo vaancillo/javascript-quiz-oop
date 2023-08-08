bd17ef5eb9cf230e3b90a67fbd4622a84bb0db27
vancillo <yaredbane@gmail.com>
2023-08-08 12:55:45 -0600

    v-1.2 adding the questions

---
+++ app.js
@@ -0,0 +1,3 @@
+import { data } from './data/data.js'
+
+console.log(data)
---
+++ data/data.js
@@ -0,0 +1,46 @@
+export const data = [
+    {
+        question: '¿Cuál de las siguientes opciones no es un editor de codigo?',
+        choices: [
+            'Visual Studio Code',
+            'Sublime Text',
+            'Atom',
+            'Adobe Photoshop'
+        ],
+        answer: 3
+    },
+
+    {
+        question: '¿Que lenguaje NO es orientado a objetos?',
+        choices: [
+            'Java',
+            'C',
+            'C++',
+            'C#'
+        ],
+        answer: 1
+    },
+
+    {
+        question: '¿Que lenguaje NO sirve para estilizar sitios web?',
+        choices: [
+            'stylus',
+            'less',
+            'sass',
+            'PostCSS',
+            'NextCSS'
+        ],
+        answer: 'PostCSS'
+    },
+
+    {
+        question: '¿Qué estructura de control se utiliza para repetir un bloque de código un número fijo de veces?',
+        choices: [
+            'for',
+            'while',
+            'if',
+            'switch'
+        ],
+        answer: 1
+    },
+]
